7374e75cb75134accbe00c7a12247b35575adfe8
Danlu Wang <notprovided>
2020-01-14 10:01:16 +0000

    multiples35

---
+++ kyu6/multiples_3_5.rb
@@ -0,0 +1,21 @@
+def solution(number)
+a =[]
+for i in 1 ... number
+  if i % 3 == 0 or i % 5 == 0
+    a << i
+  end
+end
+return a.inject(0){|sum, x| sum += x}
+end
+
+
+
+
+
+puts solution(10)
+# , 23
+puts solution(20)
+# , 78
+
+puts solution(200)
+# , 9168
